e7a29cb755d5b408d1305b320f400ef9a410bf81
chau <hndc9x@gmail.com>
2020-11-04 23:30:12 +0700

    Register User

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose'); 
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -8,6 +9,50 @@ const userSchema = new mongoose.Schema({
         min:3,
         max:20
     },
+    lastName:{
+        type:String,
+        required : true,
+        trim : true,
+        min:3,
+        max:20
+    },
+    username:{
+        type:String,
+        required:true,
+        trim : true,
+        unique:true,
+        index:true,
+        lowercase:true
+    },
+    email:{
+        type:String,
+        required:true,
+        trim : true,
+        unique:true,
+        lowercase:true
+    },
+    hash_password:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:['user','admin'],
+        default:'admin'
+    },
+    contactNumber:{type:String},
+    pofilePicture:{type:String}
+},{timestamps:true});
+
+userSchema.virtual('password')
+.set(function(password){
+    this.hash_password = bcrypt.hashSync(password,10);
 });
 
-module.exports = mongoose.model('User');
+userSchema.method ={
+    authenticate:function(password){
+        return bcrypt.compare(password,this.hash_password);
+    }
+}
+
+module.exports = mongoose.model('User',userSchema);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const { signup } = require('../controller/user');
 const router = express.Router();
 
+
+router.post('/signup',signup)
+
 router.post('/signin',(req,res)=>{
-
-});
-
-router.get('/signup',(req,res)=>{
-
+    
 });
 
 module.exports = router;
