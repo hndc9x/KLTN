98e2601fcef0165b11e151f2aafd7a377c4c5464
chau <hndc9x@gmail.com>
2020-11-07 17:10:19 +0700

    UI Signin Admin

---
+++ src/containers/Home/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Jumbotron } from 'react-bootstrap'
+import Layout from '../../components/Layout'
+
+/**
+* @author
+* @function Home
+**/
+
+const Home = (props) => {
+  return(
+    <Layout>
+        <Jumbotron style={{margin:'5rem' ,background:'#fff'}} className ='text-center'>
+            <h1>Chau CHau</h1>
+            <p>kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+                sdasasasasasasasasasasasasask</p>
+        </Jumbotron>
+    </Layout>
+   )
+
+ }
+
+export default Home
