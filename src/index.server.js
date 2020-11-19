0b73422d04a31297560bb8f0c930dbcca4db16bc
chau <hndc9x@gmail.com>
2020-11-19 16:46:58 +0700

    add product to cart

--- src/index.server.js
+++ src/index.server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const env = require('dotenv');
 const mongoose = require('mongoose');
 const app = express();
+const path = require('path');
 
 
 //route
@@ -9,6 +10,7 @@ const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin/auth');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
 
 
 // cài đặt môi trường env
@@ -31,10 +33,12 @@ const connectDB = async()=>{
 connectDB() 
 
 app.use(express.json());
+app.use('/public',express.static(path.join(__dirname,'uploads')));
 app.use('/api',authRoutes);
 app.use('/api',adminRoutes);
 app.use('/api',categoryRoutes);
 app.use('/api',productRoutes);
+app.use('/api',cartRoutes);
 
 
 app.listen(process.env.PORT,()=>{
--- src/routes/category.js
+++ src/routes/category.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const { addCategory, getCategories, updateCategories, deleteCategories } = require('../controller/category');
 const { requireSignin, adminMiddleware } = require('../common-middleware');
-const { addCategory, getCategories } = require('../controller/category');
 const router = express.Router();
+const shortid = require('shortid');
+const path = require('path');
+const multer = require('multer');
 
-router.post('/category/create',requireSignin,adminMiddleware,addCategory);
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(path.dirname(__dirname), 'uploads'))
+    },
+    filename: function (req, file, cb) {
+      cb(null, shortid.generate() + '-' + file.originalname)
+    }
+});
 
-router.get('/category/getcategory',getCategories);
+const upload = multer({ storage });
 
-//router.post('/signin',validateSigninRequest,isRequestValidated,signin)
+
+router.post('/category/create', requireSignin, adminMiddleware, upload.single('categoryImage'), addCategory);
+router.get('/category/getcategory', getCategories);
+router.post('/category/update', upload.array('categoryImage'), updateCategories);
+router.post('/category/delete', deleteCategories);
 
 module.exports = router;
