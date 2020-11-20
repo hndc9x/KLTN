513c80ba646b73b7e8162f96eceb84511e8e874b
chau <hndc9x@gmail.com>
2020-11-20 17:20:59 +0700

    Admin Login

--- src/reducers/auth.reducers.js
+++ src/reducers/auth.reducers.js
@@ -1,7 +1,16 @@
 import { authConstants } from "../actions/constants";
 
 const initState = {
-  name: "Chau",
+    token : null,
+    user :{
+      firstName:'',
+      lastName:'',
+      email:'',
+      picture:'',
+      password:''
+    },
+    authenticate : false,
+    authenticating : false
 };
 
 export default (state = initState, action) => {
@@ -11,8 +20,17 @@ export default (state = initState, action) => {
     case authConstants.LOGIN_REQUEST:
       state = {
         ...state,
-        ...action.payload
+        authenticating:true
       };
+      break;
+    case authConstants.LOGIN_SUCCESS:
+      state = {
+        ...state,
+        user : action.payload.user,
+        token: action.payload.token,
+        authenticate : true,
+        authenticating : false
+      }
       break;
   }
   return state;
