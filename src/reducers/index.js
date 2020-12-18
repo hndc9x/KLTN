6fca4eb88742a5437d152ec37ef6853f68a93a4a
chau <hndc9x@gmail.com>
2020-12-18 16:48:32 +0700

    product store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import categoryReducer from './category.reducer';
 import productReducer from './product.reducer';
 import userReducer from './user.reducer';
 import authReducer from './auth.reducer';
+import cartReducer from './cart.reducer';
 
 const rootReducer = combineReducers({ 
     category : categoryReducer,
     product : productReducer,
     user : userReducer,
-    auth : authReducer
+    auth : authReducer,
+    cart : cartReducer
 });
 
 export default rootReducer;
