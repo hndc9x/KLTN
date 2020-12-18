a90b80afc7ac2e8aeca37040dcac7556b8c9c335
chau <hndc9x@gmail.com>
2020-12-18 14:24:26 +0700

    page api

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 //const {  } = require('../controller/category');
 const { requireSignin, adminMiddleware } = require('../common-middleware');
-const { createProduct, getProductsBySlug, getProductDetailsById } = require('../controller/product');
+const { createProduct, getProductsBySlug, getProductDetailsById ,getProducts } = require('../controller/product');
 const multer = require('multer');
 const router = express.Router();
 const shortid = require('shortid');
@@ -22,5 +22,6 @@ router.post('/product/create', requireSignin, adminMiddleware, upload.array('pro
 router.get('/products/:slug', getProductsBySlug)
 //router.get('/category/getcategory', getCategories);
 router.get('/product/:productId', getProductDetailsById);
+router.post( "/product/getProducts",requireSignin, adminMiddleware, getProducts);
 
 module.exports = router;
