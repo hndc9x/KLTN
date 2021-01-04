c91d382d230ec93ada9b5db3b766ef1b18bb5fdb
chau <hndc9x@gmail.com>
2021-01-05 05:46:01 +0700

    api send email pay

---
+++ src/controller/emailPay.js
@@ -0,0 +1,39 @@
+const nodemailer = require('nodemailer');
+
+exports.sendEmailPay = (req,res) => {
+    const transporter = nodemailer.createTransport({
+        service : 'gmail',
+        auth:{
+            user : process.env.EMAIL,
+            pass : process.env.PASS
+        }
+    });
+   // console.log(`email list = ${jsonObject["emails"]}`);
+    const mailOption = {
+        from : process.env.EMAIL,
+        to : req.body.email,
+        subject : "Thanh Toán Thành Công",
+        //text : req.body.text
+        html : `<h2>Cảm ơn đã tin tưởng vào shop chúng tôi</h2> 
+        <p>Chúc mừng bạn đã thành toán thành công</p>
+        <p>Thông tin hóa đơn</p>
+        <p>Mã hóa đơn: ${req.body.id}</p>
+        <p>Tên Khách Hàng: ${req.body.name}</p>
+        <p>Địa chỉ email: ${req.body.email}</p>
+        <p>Địa chỉ: ${req.body.address} </p>
+        <p>Loại thanh toán : ${req.body.card} </p>
+        <p>Loại thẻ: ${req.body.brand}</p>`
+    };
+    transporter.sendMail(mailOption,(error , info) => {
+        if (error) {
+            console.log(error);
+            return res.status(400).json({
+                message : error
+            });
+        }else{
+            return res.status(200).json({
+                message: "Email sent" + info.response
+              });
+        }
+    });
+}
--- src/routes/admin/email.js
+++ src/routes/admin/email.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { sendEmail } = require('../../controller/admin/email');
+const { sendEmailPay } = require('../../controller/emailPay');
 const router = express.Router();
 
 
 
 
 router.post('/admin/sendemail',sendEmail);
+
+router.post('/endEmailPay',sendEmailPay);
 
 module.exports = router;
 
